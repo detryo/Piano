e51399e5a5bedbf816964275b334c19ffe9d7c55
Cristian <chris.sedano.arenas@gmail.com>
2019-01-28 17:41:39 +0000

    Interface
    I put 13 buttons, emulate a piano

---
+++ Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Piano
+//
+//  Created by Cristian Sedano Arenas on 24/1/19.
+//  Copyright © 2019 Cristian Sedano Arenas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
