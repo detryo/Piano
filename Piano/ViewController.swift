5e30701f91210149eb9375113c390c703d6f03ae
Cristian <chris.sedano.arenas@gmail.com>
2019-02-06 16:03:49 +0000

    Piano Sounds and AVFundation
    Implement some sounds, let soundsArray, IBAction func playSound with all buttons, and config, use URL and AVAudioPlayer

--- Piano/ViewController.swift
+++ Piano/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    let soundsArray = [ "a1", "a1s", "b1", "c1", "c1s", "c2", "d1",
+                        "d1s","e1", "f1","f1s", "g1", "g1s"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func playSound(_ sender: UIButton) {
+        let idKey = sender.tag
+        let fileName = soundsArray[idKey-1]
+        
+        if let soundURL : URL = Bundle.main.url(forResource: fileName, withExtension: "mp3"){
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            } catch {
+                print(error)
+            }
+            audioPlayer.play()
+        }
+    }
 }
 
